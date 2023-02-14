cc3aaeea102b049fecae9704505237fbc1c7fca3
sander3405 <124794772+sander3405@users.noreply.github.com>
2023-02-14 04:04:49 -0500

    Update paradas.service.ts

--- src/paradas/paradas.service.ts
+++ src/paradas/paradas.service.ts
@@ -19,7 +19,7 @@ export class ParadasService {
   }
 
   update(id: number, updateParadaDto: UpdateParadaDto) {
-    return this.prisma.parada.update({
+    return this.prisma.paradas.update({
       where: { id },
       data: updateParadaDto,
     });
